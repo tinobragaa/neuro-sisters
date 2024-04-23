305de88226c17e950ff856670e2f3f9fed4a4996
alexandergrib <alexgrib@hotmail.com>
2024-04-23 19:59:12 +0100

    Update comment and user profile fields, adjust blog HTML and database settings
    A new migration file has been added to alter 'content' field in 'comment' model and 'bio' field in 'userprofile' model using CKEditor5Field. Some unused lines have been removed in blog.html and a TODO comment has been added for future enhancements. In the settings.py, database settings are altered to read from an environment variable, with SSL requirement set to False.
    
    close #13

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -94,12 +94,23 @@ WSGI_APPLICATION = 'django_settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    'default': env.db(),
+
 }
+DATABASES['default'].update(ssl_require=False)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
