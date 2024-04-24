95ebf3030cc12300299df059e69ecdd341aac076
alexandergrib <alexgrib@hotmail.com>
2024-04-24 22:28:01 +0100

    Implement user feedback messages
    This update introduces user feedback messages in various scenarios across the application. It adds Django's messaging framework in views for different user actions like adding/removing friends, deleting comments, categories and posts. In addition to that, it includes the creation of toast templates for success, error, info and warning messages which are then inserted into the application's base HTML. The purpose of these changes is to provide the user with immediate feedback regarding their actions in the application.

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,7 +6,7 @@ from user_profile.views import (display_user_profile,
 
 urlpatterns = [
     path('', display_user_profile, name='profile'),
-    path('friend/<int:user_id>', display_friend_profile, name='friend_profile'),
+    path('friend/<int:friend_id>', display_friend_profile, name='friend_profile'),
     path('friend/add/<int:user_id>', add_friend, name='add_friend'),
     path('friend/remove/<int:friend_id>', remove_friend, name='remove_friend'),
 ]
